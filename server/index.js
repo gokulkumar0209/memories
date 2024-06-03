185b94ef65e306a93fff32b3b9c441cb43da4605
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-06-04 00:10:25 +0530

    Router created

--- server/index.js
+++ server/index.js
@@ -3,19 +3,21 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
 
 dotenv.config({ path: "./.env" });
 
 const app = express();
+app.use("/posts", postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
-console.log(CONNECTION_URL);
+// console.log(CONNECTION_URL);
 const PORT = process.env.PORT || 3000;
-console.log(PORT);
+// console.log(PORT);
 
 mongoose
 	.connect(CONNECTION_URL)
