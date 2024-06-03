1eaefb7423d34d1f70a3bca1987a71212be7e223
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-06-03 23:04:53 +0530

    Corrected the mongodb connection issue

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
-const CONNECTION_URL =
-"mongodb+srv://Reelrover:Reel@54321@cluster0.r0ii96a.mongodb.net/"
 
+const CONNECTION_URL = process.env.CONNECTION_URL;
 console.log(CONNECTION_URL);
 const PORT = process.env.PORT || 3000;
-console.log(PORT)
+console.log(PORT);
+
 mongoose
 	.connect(CONNECTION_URL)
 	.then(() =>
